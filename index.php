c29d28f3ba5beb75c404b00c8ec18592400058bc
cullenm <cmarchenko@csumb.edu>
2017-09-20 20:42:19 +0000

    finished1?

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
+<?php
+    include 'inc/functions.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
+        <style>
+            @import url("/lab/777/css/styles.css");
+        </style>
         <title> 777 Slot Machine </title>
     </head>
     <body>
         
-        <img src="/lab/777/img/cherry.png"/>
-        
-        <?php
-            $randomValue = rand(0,2);
-            if ($randomValue == 0) {
-                echo '<img src="/lab/777/img/seven.png" alt="seven" title="Seven" width="70" />';
-            } else if ($randomValue == 1) {
-                echo '<img src="/lab/777/img/cherry.png" alt="cherry" title="Cherry" width="70" />';
-            } else if ($randomValue == 2) {
-                echo '<img src="/lab/777/img/lemon.png" alt="lemon" title="Lemon" width="70" />';
-            }
-        ?>
+        <div id="main">
+            <?php
+                play();
+            ?>
+            
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
         
     </body>
 </html>
